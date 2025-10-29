4c719de774aa8f7d738ad23289f50241dedd781f
ababic20 <ababic20@foi.hr>
2025-10-29 22:49:38 +0100

    fix#4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
-    emptyOutDir: true,
   },
-  publicDir: "public",
-
 });
-
