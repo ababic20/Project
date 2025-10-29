813e6c0cbd0c890bb58131b98697bdc40fbd354a
ababic20 <ababic20@foi.hr>
2025-10-29 22:42:07 +0100

    fix #2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import fs from "fs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  publicDir: "public",
+
+});
+
