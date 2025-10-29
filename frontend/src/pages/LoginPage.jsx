d4877e942fa29ca0203a82fd0937d52a467215f3
ababic20 <ababic20@foi.hr>
2025-10-29 22:27:02 +0100

    ups

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -12,7 +12,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:9000/login", {
+      const response = await fetch("https://project-kj3g.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
